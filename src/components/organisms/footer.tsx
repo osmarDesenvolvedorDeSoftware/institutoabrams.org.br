6da19b5335e87d3be943720549da408de3f9de6d
Leo Alves <leoalves0698@gmail.com>
2025-10-02 18:13:47 -0300

    style: change tag styles

--- src/components/organisms/footer.tsx
+++ src/components/organisms/footer.tsx
@@ -2,8 +2,8 @@ import { FooterInfo } from '../molecule';
 
 export const FooterComponent = () => {
   return (
-    <footer className="relative bg-footer bg-no-repeat bg-center bg-cover h-[548px] grid grid-cols-12">
-      <div className="absolute bg-dark/95 inset-0 z-10" />
+    <footer className="relative bg-dark h-[548px] grid grid-cols-12">
+      <div className="absolute  bg-footer opacity-5 bg-no-repeat bg-center bg-cover inset-0 z-10" />
       <FooterInfo>
         <FooterInfo.Description />
       </FooterInfo>
