3d2c252432b7905b800064e37d0534cbdbeaf4fa
Leo Alves <leoalves0698@gmail.com>
2025-10-05 14:19:36 -0300

    refactor: remove footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { FooterComponent } from '@/components/organisms/footer';
-
 export default function Home() {
-  return (
-    <div>
-      <FooterComponent />
-    </div>
-  );
+  return <div></div>;
 }
