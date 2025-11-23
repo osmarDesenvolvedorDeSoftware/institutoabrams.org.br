2d6c683f1deace9bdbb3119b990d40b8d324f5e4
Danilo Machado de Sousa <danilomsou@gmail.com>
2025-11-23 16:49:23 -0300

    refactor: move icons and images to a assets folder

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,12 @@
 export const icons = {
-  'location-icon': '/icons/location.svg',
-  mail: '/icons/mail.svg',
-  phone: '/icons/phone.svg',
-  facebook: '/icons/facebook.svg',
-  instagram: '/icons/instagram.svg',
-  linkedin: '/icons/linkedin.svg',
-  youtube: '/icons/youtube.svg',
-  twitter: '/icons/twitter.svg',
+  "location-icon": "/assets/icons/location.svg",
+  mail: "/assets/icons/mail.svg",
+  phone: "/assets/icons/phone.svg",
+  facebook: "/assets/icons/facebook.svg",
+  instagram: "/assets/icons/instagram.svg",
+  linkedin: "/assets/icons/linkedin.svg",
+  youtube: "/assets/icons/youtube.svg",
+  twitter: "/assets/icons/twitter.svg",
 } as const;
 
 export type IconName = keyof typeof icons;
--- src/components/molecules/ImageGallerySection.tsx
+++ src/components/molecules/ImageGallerySection.tsx
@@ -1,13 +1,4 @@
-import React from 'react';
-import subtractImage from './Subtract.png';
-import galleryImage from './image.png';
-
-interface ImageModule {
-  src: string;
-}
-
-const resolveImageSrc = (image: string | ImageModule): string =>
-  typeof image === 'string' ? image : image.src;
+import React from "react";
 
 interface ImageGallerySectionProps {}
 
@@ -17,22 +8,24 @@ const ImageGallerySection: React.FC<ImageGallerySectionProps> = () => {
       <div
         className="relative h-[348px] w-[482.48px] rounded-lg bg-blue-500 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: `url(${resolveImageSrc(subtractImage)})`,
+          backgroundImage: `url(/assets/images/Subtract.png)`,
         }}
       >
         <div className="absolute top-[128px] left-[-128px] z-0 h-[267px] w-[282px] rounded-[22px] bg-white shadow-md" />
         <div
           className="absolute top-[150px] left-[-150px] z-[1] h-[267px] w-[282px] rounded-[22px] bg-cover bg-center bg-no-repeat"
           style={{
-            backgroundImage: `url(${resolveImageSrc(galleryImage)})`,
+            backgroundImage: `url(/assets/images/image.png)`,
           }}
         />
         <div className="absolute top-[265px] left-[320px] z-0 h-[267px] w-[282px] rounded-lg border border-amber-50 bg-white" />
-       
+
         <div className="absolute top-[330px] left-[90px] z-[2] h-[117px] w-[209px] rounded-lg bg-[#ED632F] p-3">
           <span className="flex items-center gap-2">
             <div>
-              <p className="text-[46px] font-bold leading-none text-white">50</p>
+              <p className="text-[46px] font-bold leading-none text-white">
+                50
+              </p>
             </div>
             <p className="text-[20px] font-medium text-white">Lakh</p>
           </span>
